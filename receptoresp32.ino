9a9cd5204f01c5bfff46f1839ae89add1ed7dd69
osuamists <134652319+osuamists@users.noreply.github.com>
2024-03-08 15:14:12 -0300

    Create receptoresp32.ino

---
+++ receptoresp32.ino
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#include <IRrecv.h>
+#include <IRremoteESP8266.h>
+#include <IRac.h>
+#include <IRtext.h>
+#include <IRutils.h>
+
+const uint16_t kRecvPin = 2;
+const uint32_t kBaudRate = 115200;
+const uint16_t kCaptureBufferSize = 1024;
+
+#if DECODE_AC
+const uint8_t kTimeout = 50;
+#else   // DECODE_AC
+// Suits most messages, while not swallowing many repeats.
+const uint8_t kTimeout = 15;
+#endif  // DECODE_AC
+
+const uint16_t kMinUnknownSize = 12;
+
+// Legacy (No longer supported!)
+//
+// Change to `true` if you miss/need the old "Raw Timing[]" display.
+#define LEGACY_TIMING_INFO false
+// ==================== end of TUNEABLE PARAMETERS ====================
+
+// Use turn on the save buffer feature for more complete capture coverage.
+IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
+decode_results results;  // Somewhere to store the results
+
+// This section of code runs only once at start-up.
+void setup() {
+#if defined(ESP8266)
+  Serial.begin(kBaudRate, SERIAL_8N1, SERIAL_TX_ONLY);
+#else  // ESP8266
+  Serial.begin(kBaudRate, SERIAL_8N1);
+#endif  // ESP8266
+  while (!Serial)  // Wait for the serial connection to be establised.
+    delay(50);
+  Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", kRecvPin);
+#if DECODE_HASH
+  // Ignore messages with less than minimum on or off pulses.
+  irrecv.setUnknownThreshold(kMinUnknownSize);
+#endif  // DECODE_HASH
+  irrecv.enableIRIn();  // Start the receiver
+}
+
+// The repeating section of the code
+void loop() {
+  // Check if the IR code has been received.
+  if (irrecv.decode(&results)) {
+    // Display a crude timestamp.
+    uint32_t now = millis();
+    Serial.printf(D_STR_TIMESTAMP " : %06u.%03u\n", now / 1000, now % 1000);
+    // Check if we got an IR message that was to big for our capture buffer.
+    if (results.overflow)
+      Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
+    // Display the library version the message was captured with.
+    Serial.println(D_STR_LIBRARY "   : v" _IRREMOTEESP8266_VERSION_ "\n");
+    // Display the basic output of what we found.
+    Serial.print(resultToHumanReadableBasic(&results));
+    // Display any extra A/C info if we have it.
+    String description = IRAcUtils::resultAcToString(&results);
+    if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
+    yield();  // Feed the WDT as the text output can take a while to print.
+#if LEGACY_TIMING_INFO
+    // Output legacy RAW timing info of the result.
+    Serial.println(resultToTimingInfo(&results));
+    yield();  // Feed the WDT (again)
+#endif  // LEGACY_TIMING_INFO
+    // Output the results as source code
+    Serial.println(resultToSourceCode(&results));
+    Serial.println();    // Blank line between entries
+    yield();             // Feed the WDT (again)
+  }
+}
